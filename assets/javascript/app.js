cc6391d08d8c93f23b23fa902741b91a205183a8
Jake David <jakedgmu13@gmail.com>
2019-04-06 11:01:59 -0400

    blah

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJxqjP1ozV7Rdxi3KCbiI5WNkymJXqt5Y",
+    authDomain: "super-rad-project-alpha.firebaseapp.com",
+    databaseURL: "https://super-rad-project-alpha.firebaseio.com",
+    projectId: "super-rad-project-alpha",
+    storageBucket: "super-rad-project-alpha.appspot.com",
+    messagingSenderId: "906425428681"
+  };
+  firebase.initializeApp(config);
+
+
+  
